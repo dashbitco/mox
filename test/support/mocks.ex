47e60dc685c63c7fe77813e953001670e037ee23
Adriano Mitre <adriano.mitre@gmail.com>
2020-08-07 15:06:13 -0300

    conform to mix format --check-formatted

--- test/support/mocks.ex
+++ test/support/mocks.ex
@@ -1,6 +1,10 @@
 Mox.defmock(CalcMock, for: Calculator)
 Mox.defmock(SciCalcMock, for: [Calculator, ScientificCalculator])
-Mox.defmock(SciCalcMockWithoutOptional, for: [Calculator, ScientificCalculator], skip_optional_callbacks: true)
+
+Mox.defmock(SciCalcMockWithoutOptional,
+  for: [Calculator, ScientificCalculator],
+  skip_optional_callbacks: true
+)
 
 Mox.defmock(MyMockWithoutModuledoc, for: Calculator)
 Mox.defmock(MyMockWithFalseModuledoc, for: Calculator, moduledoc: false)
